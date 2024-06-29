50eef006be1f1d19a1b0173ff73bc746eb12f769
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-06-29 16:32:32 +0530

    Update server.js file to import the auth router

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import authRoute from "./routes/auth.js";
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use("/api/auth", authRoute);
 
 app.listen(8000, () => console.log("Server is running on Port 8000"));
