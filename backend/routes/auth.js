372ea3c69563c255ba6e0010bc70e9a20800aaf8
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-06-29 16:35:04 +0530

    Update auth.js with the code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/signup', (req, res)=> {
+router.post('/signup', (req, res)=> {
     res.json({
         data: "You hit the signup endpoint",
+    });
+});
+
+router.post('/login', (req, res)=> {
+    res.json({
+        data: "You hit the login endpoint",
+    });
+});
+
+router.post('/logout', (req, res)=> {
+    res.json({
+        data: "You hit the logout endpoint",
     });
 });
 
