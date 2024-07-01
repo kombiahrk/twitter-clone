c40a7e5cfaec559d207e0814ed5a9069502b492d
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-01 19:58:12 +0530

    Update userRouter.js
    By including followOrUnFollow function from userController

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { isAuthorized } from "../middleware/authCheck.js";
-import { getUserProfile } from "../controllers/userController.js";
+import { getUserProfile, followOrUnFollow } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", isAuthorized, getUserProfile);
+
+router.post("/follow/:id", isAuthorized, followOrUnFollow);
 
 export default router;
