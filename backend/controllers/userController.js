e7c25f397c26c454c49e3f069b7a367544cb215d
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-01 19:55:16 +0530

    Update userContoller.js
    By creating followOrUnFollow() function and update with code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,3 +17,40 @@ export const getUserProfile = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 }
+
+export const followOrUnFollow = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userToFollowOrUnFollow = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+
+        if (id == req.user._id.toString()) {
+            console.log("You can't follow or unfollow yourself");
+            return res.status(400).json({ error: "You can't follow or unfollow yourself" });
+        }
+
+        if (!userToFollowOrUnFollow || !currentUser) {
+            console.log("User not found");
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        const isFollowing = currentUser.following.includes(id);
+
+        if (isFollowing) {
+            //unfollow the user
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } })
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } })
+            console.log("User unfollowed successfully");
+            res.status(200).json({ message: "User unfollowed successfully" });
+        } else {
+            //follow the user
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } })
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } })
+            console.log("User followed successfully");
+            res.status(200).json({ message: "User followed successfully" });
+        }
+    } catch (error) {
+        console.log("Error in followOrUnfollow controller:", error.message)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
