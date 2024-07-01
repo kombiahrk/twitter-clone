d05ea3e88c6ca2ec9ab8f05c8b6477ec28174cd1
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-01 15:43:19 +0530

    Update the server.js
    Update the server.js by import cookieParser and use it

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import donenv from "dotenv";
 import authRoute from "./routes/auth.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 donenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 
