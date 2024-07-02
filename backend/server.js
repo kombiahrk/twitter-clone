2981583e424893ace138e9143f5a852412e7eba7
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-02 14:58:51 +0530

    Update server.js
    typofix()

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
