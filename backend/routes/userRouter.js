425c27b52174bffa3ee3072461ae2c14b1e64311
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-02 17:43:27 +0530

    Update  userRouter.js
    by importing and use updateUser from userController

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuthorized } from "../middleware/authCheck.js";
-import { getUserProfile, followOrUnFollow, getSuggestedUser } from "../controllers/userController.js";
+import { getUserProfile, followOrUnFollow, getSuggestedUser, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get("/profile/:username", isAuthorized, getUserProfile);
 router.get("/suggested", isAuthorized, getSuggestedUser);
 
 router.post("/follow/:id", isAuthorized, followOrUnFollow);
+
+router.post("/update", isAuthorized, updateUser);
 
 export default router;
