5170560d2e2f43af4f36bbdc7a81ba2170f26525
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-04 12:51:24 +0530

    Update server.js
    to include postRouter

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRouter.js";
 import userRoutes from "./routes/userRouter.js";
+import postRoutes from "./routes/postRouter.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
