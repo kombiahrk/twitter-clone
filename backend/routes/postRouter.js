125d44ec2dc8f76a33034fde19814daeee0f1299
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-04 14:25:08 +0530

    Update postRouter.js
    By importing and use deletePost from post Controller

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { isAuthorized } from "../middleware/authCheck.js";
-import { createPost } from "../controllers/postController.js";
+import { createPost, deletePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", isAuthorized, createPost);
+
+router.delete("/:id", isAuthorized, deletePost);
 
 export default router;
