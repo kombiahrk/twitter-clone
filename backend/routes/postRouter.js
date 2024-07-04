6ba3f4832b20cca4f2f215915e7c77205f107ea8
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-04 15:54:47 +0530

    Update postRouter.js
    By importing and use commentOnPost from post Controller

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import { isAuthorized } from "../middleware/authCheck.js";
-import { createPost, deletePost } from "../controllers/postController.js";
+import { commentOnPost, createPost, deletePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", isAuthorized, createPost);
 
 router.delete("/delete/:id", isAuthorized, deletePost);
+
+router.post("/comment/:id", isAuthorized, commentOnPost);
 
 export default router;
