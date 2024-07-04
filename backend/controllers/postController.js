de74717460ce69e6cab116a42b81ad9606f6e53a
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-04 14:24:41 +0530

    Update postController
    By adding deletePost function

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -38,3 +38,30 @@ export const createPost = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 }
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            console.log("Post not found");
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        if (post.user.toString() !== req.user._id.toString()) {
+            console.log("You are not authorized to delete this post");
+            return res.status(401).json({ error: "You are not authorized to delete this post" });
+        }
+
+        if (post.img) {
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        console.log("Post deleted successfully");
+        res.status(200).json({ message: "Post deleted successfully" });
+    } catch (error) {
+        console.log("Error in deletePost controller:", error.message)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
