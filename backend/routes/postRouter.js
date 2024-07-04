e4be4e92165278ad49622cb495c77763126d2eec
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-04 16:20:19 +0530

    Update postRouter.js
    By importing and use likeUnlikePost from post Controller

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { isAuthorized } from "../middleware/authCheck.js";
-import { commentOnPost, createPost, deletePost } from "../controllers/postController.js";
+import { commentOnPost, createPost, deletePost, likeUnlikePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -10,5 +10,7 @@ router.post("/create", isAuthorized, createPost);
 router.delete("/delete/:id", isAuthorized, deletePost);
 
 router.post("/comment/:id", isAuthorized, commentOnPost);
+
+router.post("/like/:id", isAuthorized, likeUnlikePost);
 
 export default router;
