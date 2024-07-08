859fdbf1058ba9ad4b5f0a8a1c7e03d3bee08586
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-08 11:40:19 +0530

    Update App.jsx to import and use the HomePage, SignUpPage & LoginPage Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
+
 function App() {
 
   return (
-    <>
-      <p className='text-5xl text-red-500 bg-blue-300'>Hello from App.jsx</p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </div>
   )
 }
 
