4637bb98a51839e0754aa02c13a8db31a8346f0c
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-10 15:45:17 +0530

    Update server.js in backend to increase the payload request

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors()); // Enable CORS for all origins
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
